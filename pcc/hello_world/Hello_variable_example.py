610745d98dd739f84c5c93f90b5ec53b62029180
prpl <bnicoletta@idevops.io>
2022-11-22 19:08:43 -0800

    python_examples

---
+++ pcc/hello_world/Hello_variable_example.py
@@ -0,0 +1,4 @@
+# use variables to be fancy and make things easier to manage
+message = ("Hello World")
+message2 = ("Nice to meet you")
+print(message , message2)
---
+++ pcc/hello_world/hello_world.py
@@ -0,0 +1,2 @@
+# print is used to print text just make sure you remember to use ("text goes here")
+print("Hello world!")
